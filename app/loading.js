648901f95d5b67225dabed335f74116f17676e6b
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-03-23 21:58:33 +0600

    finalise

--- app/loading.js
+++ app/loading.js
@@ -1,11 +1,31 @@
 import React from "react";
 
-export default function loading() {
+export default function Loading() {
   return (
-    <section className="w-11/12 lg:w-10/12 max-w-7xl mx-auto py-10">
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:grid-cols-4 my-4 lg:my-10">
-        loading...
+    <div className=" w-full lg:w-full grid grid-cols-2 gap-4 lg:grid-cols-3 my-4 lg:my-10">
+      <LoaderCard />
+      <LoaderCard />
+      <LoaderCard />
+      <LoaderCard />
+      <LoaderCard />
+      <LoaderCard />
+    </div>
+  );
+}
+
+function LoaderCard() {
+  return (
+    <div className="ring-1 p-2  rounded grid shadow-md ring-black/10  w-full animate-pulse">
+      <div className="relative delay-150 lg:w-full rounded-md h-[205px] lg:h-[310px] bg-[#ded8f8]/40  bg-cover bg-center transition-all duration-300 ease-in-out transform"></div>
+      <div className="h-4 rounded bg-slate-200/50 mt-2"></div>
+      <div className="h-1 rounded bg-slate-200/50 mt-2"></div>
+      <div className="h-1 rounded bg-slate-200/50 mt-1"></div>
+      <div className="h-1 rounded bg-slate-200/50 mt-1"></div>
+      <div className="h-1 rounded bg-slate-200/50 mt-1"></div>
+      <div className="flex gap-2">
+        <div className="w-1/2 h-2 rounded bg-slate-400/30"></div>
+        <div className="w-1/2 h-2 bg-slate-400/30 rounded"></div>
       </div>
-    </section>
+    </div>
   );
 }
--- app/products/[id]/loading.js
+++ app/products/[id]/loading.js
@@ -1,7 +1,22 @@
 export default function Loading() {
   return (
-    <div className=" w-full lg:w-full grid grid-cols-2 gap-4 lg:grid-cols-3 my-4 lg:my-10">
-      loading...
-    </div>
+    <section className="bg-[#fafaf2] h-full py-20">
+      <div className="w-11/12 lg:w-8/12 max-w-7xl mx-auto flex flex-col gap-12 lg:flex-row items-center justify-between animate-pulse ">
+        <div className="h-[50vh] bg-slate-200 w-full "></div>
+        <div className="w-full space-y-5">
+          <div className="h-10 rounded-md w-full bg-slate-300 mb-10"></div>
+          <div className="h-3 w-full bg-slate-300"></div>
+          <div className="h-3 w-full bg-slate-300"></div>
+          <div className="h-3 w-full bg-slate-300"></div>
+          <div className="h-3 w-full bg-slate-300"></div>
+          <div className="h-3 w-full bg-slate-300"></div>
+          <div className="h-3 w-full bg-slate-300"></div>
+          <div className="flex gap-3 justify-start w-1/2 items-end">
+            <div className="h-5 rounded-full w-full bg-slate-300"></div>
+            <div className="rounded-full h-6 w-full bg-slate-300"></div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
