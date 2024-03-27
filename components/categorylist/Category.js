41ff6854d01c3d4f2e355475148bc3abc3478c46
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-03-27 13:07:59 +0600

    add dynamic category

--- components/categorylist/Category.js
+++ components/categorylist/Category.js
@@ -1,8 +1,10 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import Products from "../../data/products.json";
+import { useEffect, useState } from "react";
 export default function Category() {
+  const [categories, setCategories] = useState([]);
   const pathname = usePathname();
   const formatCategoryName = (name) => {
     return "category/" + name.toLowerCase();
@@ -12,10 +14,23 @@ export default function Category() {
     return pathname === `/${formatCategoryName(name)}`;
   };
 
+  useEffect(() => {
+    const categorySet = new Set();
+    Products.forEach((product) => {
+      categorySet.add(product.category);
+    });
+
+    // Convert Set back to an array
+    const categoryArray = Array.from(categorySet);
+
+    setCategories(categoryArray);
+  }, []);
+
   return (
     <div className="flex items-center justify-between lg:block lg:w-4/12 my-10 lg:my-0 lg:mt-4 sticky top-20">
       <p className="text-xl font-bold py-2">Categories</p>
       <hr />
+
       <Link
         className={`hover:border-b border-black block h-9 p-2 box-border mt-4 duration-500 py-2 ${
           pathname === "/category" ? "active" : ""
@@ -24,46 +39,17 @@ export default function Category() {
       >
         All
       </Link>
-      <Link
-        className={`hover:border-b border-black block h-9 p-2 box-border mt-4 duration-500 ${
-          isActive("Smartphones") ? "active" : ""
-        }`}
-        href={`/${formatCategoryName("Smartphones")}`}
-      >
-        Smartphones
-      </Link>
-      <Link
-        className={`hover:border-b border-black block h-9 p-2 box-border mt-4 duration-500 ${
-          isActive("Laptops") ? "active" : ""
-        }`}
-        href={`/${formatCategoryName("Laptops")}`}
-      >
-        Laptops
-      </Link>
-      <Link
-        className={`hover:border-b border-black block h-9 p-2 box-border mt-4 duration-500 ${
-          isActive("Fragrances") ? "active" : ""
-        }`}
-        href={`/${formatCategoryName("Fragrances")}`}
-      >
-        Fragrances
-      </Link>
-      <Link
-        className={`hover:border-b border-black block h-9 p-2 box-border mt-4 duration-500 ${
-          isActive("Skincare") ? "active" : ""
-        }`}
-        href={`/${formatCategoryName("Skincare")}`}
-      >
-        Skincare
-      </Link>
-      <Link
-        className={`hover:border-b border-black block h-9 p-2 box-border mt-4 duration-500 ${
-          isActive("Groceries") ? "active" : ""
-        }`}
-        href={`/${formatCategoryName("Groceries")}`}
-      >
-        Groceries
-      </Link>
+      {categories.map((category) => (
+        <Link
+          key={category}
+          className={`hover:border-b border-black block h-9 p-2 box-border mt-4 duration-500 ${
+            isActive(category) ? "active" : ""
+          }`}
+          href={`/${formatCategoryName(category)}`}
+        >
+          {category}
+        </Link>
+      ))}
     </div>
   );
 }
