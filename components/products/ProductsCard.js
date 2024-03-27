ae38fe758ef7da9f8dc1519ec99f831fa64fa296
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-03-27 13:14:32 +0600

    add img and desc link to navigate from card

--- components/products/ProductsCard.js
+++ components/products/ProductsCard.js
@@ -4,11 +4,12 @@ import Link from "next/link";
 export default function ProductsCard({ product = {} }) {
   return (
     <div className="ring-1 p-2  rounded grid shadow-md ring-black/10 hover:scale-105 transition-transform duration-500 hover:shadow-xl overflow-hidden fade-in  w-full">
-      <div
-        className="relative delay-150 lg:w-full rounded-md h-[205px] lg:h-[310px] bg-[#f8f8f8]  bg-cover bg-center transition-all duration-300 ease-in-out transform"
-        style={{ backgroundImage: `url(${product?.thumbnail})` }}
-      ></div>
-
+      <Link href={`/products/${product?.id}`}>
+        <div
+          className="relative delay-150 lg:w-full rounded-md h-[205px] lg:h-[310px] bg-[#f8f8f8]  bg-cover bg-center transition-all duration-300 ease-in-out transform"
+          style={{ backgroundImage: `url(${product?.thumbnail})` }}
+        ></div>
+      </Link>
       <h2 className="text-sm lg:text-base mt-2">
         <Link className="text-base font-bold" href={`/products/${product?.id}`}>
           {product?.title}
@@ -22,7 +23,9 @@ export default function ProductsCard({ product = {} }) {
           </Link>
         </span>
       </h2>
-      <p className="text-[#919090] text-sm ">{product?.description}</p>
+      <Link href={`/products/${product?.id}`}>
+        <p className="text-[#919090] text-sm ">{product?.description}</p>
+      </Link>
 
       <p className="text-rose-600 text-sm mt-4">
         <span className="text-[#919090] line-through">
