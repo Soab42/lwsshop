20dd4160f08c7ecac7f4c46087ae4339b1b7cddb
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-03-27 17:38:17 +0600

    add discount on card

--- components/products/ProductsCard.js
+++ components/products/ProductsCard.js
@@ -3,13 +3,20 @@ import Link from "next/link";
 
 export default function ProductsCard({ product = {} }) {
   return (
-    <div className="ring-1 p-2  rounded grid shadow-md ring-black/10 hover:scale-105 transition-transform duration-500 hover:shadow-xl overflow-hidden fade-in  w-full">
+    <div className="ring-1 p-2  rounded grid shadow-md ring-black/10 hover:scale-105 transition-transform duration-500 overflow-hidden fade-in  w-full group/disc">
       <Link href={`/products/${product?.id}`}>
         <div
-          className="relative delay-150 lg:w-full rounded-md h-[205px] lg:h-[310px] bg-[#f8f8f8]  bg-cover bg-center transition-all duration-300 ease-in-out transform"
+          className="relative delay-150 lg:w-full rounded-md h-[205px] lg:h-[310px] bg-[#f8f8f8]  bg-cover bg-center transition-all duration-300 ease-in-out transform overflow-hidden"
           style={{ backgroundImage: `url(${product?.thumbnail})` }}
-        ></div>
+        >
+          <div className="w-full h-20 duration-500 bg-gradient-to-t from-rose-500 via-rose-700/60 to-transparent absolute -bottom-10 invisible group-hover/disc:visible group-hover/disc:bottom-0 flex items-end justify-center duration-500">
+            <p className="text-2xl text-white font-bold">
+              {product.discountPercentage}% discount
+            </p>
+          </div>
+        </div>
       </Link>
+
       <h2 className="text-sm lg:text-base mt-2">
         <Link className="text-base font-bold" href={`/products/${product?.id}`}>
           {product?.title}
